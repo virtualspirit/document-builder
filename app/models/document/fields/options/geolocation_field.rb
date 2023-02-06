9063f380696cffe30f7109a1d6f48e7ee975d21c
ihsan <ihsaneddin@gmail.com>
2023-02-06 15:56:24 +0700

    updates

--- app/models/document/fields/options/geolocation_field.rb
+++ app/models/document/fields/options/geolocation_field.rb
@@ -8,6 +8,7 @@ module Document
       validates :fill_method,
                 presence: true,
                 inclusion: { in: ['automatic', 'automatic_with_location', 'manual'], allow_blank: true }
+      # validates :location_field_suffix_name, presence: true
 
     end
   end
