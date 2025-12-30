1d7beaded9e6d9a72c521e4f14550a097239c96a
Gilang Ramadan <mgrware@gmail.com>
2025-12-30 16:08:01 +0700

    fix

--- app/models/document/form.rb
+++ app/models/document/form.rb
@@ -39,13 +39,41 @@ module Document
 
     after_create :auto_create_default_section
 
+    def virtual_fields instance, _fields = nil
+      _fields ||= fields
+      _fields.map do |field|
+        vp = present_virtual_field(field, target: instance)
+        if field.nested_form && vp.value
+          if vp.multiple_nested_form?
+            field.nested_form.virtual_fields = []
+            vp.value.each do |nested_instance|
+              field.nested_form.virtual_fields << virtual_fields(nested_instance, field.nested_form.fields)
+            end
+          else
+            field.nested_form.virtual_fields = virtual_fields vp.value_for_preview, field.nested_form.fields
+          end
+        end
+        vp
+      end.reject(&:access_hidden?)
+    end
+
     private
 
-      def auto_create_default_section
-        if sections.blank?
-          sections.create! title: I18n.t("defaults.section.title"), headless: true
-        end
+    def auto_create_default_section
+      if sections.blank?
+        sections.create! title: I18n.t("defaults.section.title"), headless: true
       end
+    end
 
+    protected
+
+    def present_virtual_field(model, options = {})
+      klass = options.delete(:presenter_class) || "#{model.class}Presenter".constantize
+      presenter = klass.new(model, self, options)
+
+      yield(presenter) if block_given?
+
+      presenter
+    end
   end
 end
